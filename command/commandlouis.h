3cc1aca31d809bb0da9aa3ab5c36a7fb42222d22
Louis Cahyadi <louiscahyadi23@gmail.com>
2018-11-16 00:56:18 +0700

    Update commandlouis.h

--- command/commandlouis.h
+++ command/commandlouis.h
@@ -2,21 +2,10 @@
 #include "../ADT/point.h"
 #include "../ADT/matriks.h"
 #include "../ADT/stackt.h"
+#include "../ADT/tipebentukan.h"
 
 #ifndef COMMANDLOUIS_H
 #define COMMANDLOUIS_H
-
-typedef struct {
-	Stack FoodStack; /* tabel penyimpan elemen */
-	Stack Hand;  /* alamat TOP: elemen puncak */
-    POINT Posisi; // posisi kolomg
-    int room; // ruang mana
-} Player;
-
-#define FoodStack(P) (P).FoodStack
-#define Hand(P) (P).Hand
-#define Posisi(P) (P).Posisi
-#define Room(P) (P).room
 
 void Recipe();
 //Menampilkan resep makanan
