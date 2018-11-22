133e3a4fe226fb11634472271b5a1d0e7de575ae
boasmarbun <38323100+boasmarbun@users.noreply.github.com>
2018-11-22 21:30:33 +0700

    Add files via upload

--- ADT/tipebentukan.h
+++ ADT/tipebentukan.h
@@ -1,6 +1,13 @@
 #ifndef TIPEBENTUKAN_H
 #define TIPEBENTUKAN_H
 
+#include "boolean.h"
+#include "point.h"
+//#include <stdlib.h>
+//#include <stdio.h>
+#include "queue.h"
+#include "stackt.h"
+
 typedef struct {
 	Stack FoodStack; /* tabel penyimpan elemen */
 	Stack Hand;  /* alamat TOP: elemen puncak */
@@ -13,56 +20,15 @@ typedef struct {
 #define Posisi(P) (P).Posisi
 #define Room(P) (P).room
 
+Queue AntrianLuar; //antrian di luar
 
-#define IdxMax 9
-#define IdxMin 0
-#define IdxUndef -999
-
-/* Definisi elemen dan koleksi objek */
-typedef char KataArray[10];
-typedef int IdxType;  /* type indeks */
-typedef KataArray ArrayElType;   /* type elemen tabel */
-typedef struct {
-	ArrayElType TI[IdxMax+1]; /* memori tempat penyimpan elemen (container) */
-	int Neff; /* >=0, banyaknya elemen efektif */
-} TabInt;
-
-//Selektor
-#define Neff(T) (T).Neff
-#define TI(T) (T).TI
-#define ArrayElmt(T,i) (T).TI[(i)]
+POINT DaftarMeja[5]; //Letak koordinat dari meja
 
 
-typedef struct {
-	int HH; /* integer [0..23] */
-	int MM; /* integer [0..59] */
-	int SS; /* integer [0..59] */
-} JAM;
+Kata DaftarOrder[13]; //Daftar makanan yang dipesan, meja sesuai indeks
 
-/* *** Notasi Akses: selektor JAM *** */
-#define Hour(J) (J).HH
-#define Minute(J) (J).MM
-#define Second(J) (J).SS
+Customer arrayCust[13]; //Salinan customer ke meja
 
-
-
-
-
-#define BrsMin 1
-#define BrsMax 10
-#define KolMin 1
-#define KolMax 10
-
-typedef int indeks; /* indeks baris, kolom */
-typedef char MatriksElType;
-typedef struct {
-	MatriksElType Mem[BrsMax+1][KolMax+1];
-    int NBrsEff; /* banyaknya/ukuran baris yg terdefinisi */
-	int NKolEff; /* banyaknya/ukuran kolom yg terdefinisi */
-} MATRIKS;
-#define NBrsEff(M) (M).NBrsEff
-#define NKolEff(M) (M).NKolEff
-#define MatriksElmt(M,i,j) (M).Mem[(i)][(j)]
 
 
 
