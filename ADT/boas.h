23168d1a77b873e8f512829a3a0a8594a3e0a5be
boasmarbun <38323100+boasmarbun@users.noreply.github.com>
2018-11-25 16:40:17 +0700

    Add files via upload

--- ADT/boas.h
+++ ADT/boas.h
@@ -1,14 +1,16 @@
 #ifndef BOAS_H
 #define BOAS_H
 
-#include "boolean.h"
-#include "point.h"
+#include "../ADT/boolean.h"
+#include "../ADT/point.h"
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-#include "queue.h"
-#include "stackt.h"
-#include "listrek.h"
-#include "bintree.h"
+#include "../ADT/queue.h"
+#include "../ADT/graph.h"
+#include "../ADT/stackt.h"
+#include "../ADT/listrek.h"
+#include "../ADT/bintree.h"
 
 
 
@@ -17,19 +19,29 @@ boolean IsReachable(Player P, int x, int y, int jarak);
 // Player P, x,y menyatakan koordinat tujuan, jarak menyatakan jarak yang kita mau, utk room 2, utk kitchen 1
 void Order(Player P);  
 void Put(Player *P);
-void Take(Player *P, POINT *pts );
+void Take(Player *P);
 void CH(Player *P);
 void CT(Player *P);
 void Place(Player P);
 void Give(Player *P);   
 void Recipe();
-void NextTick(Player P);
+void NextTick(Player *P);
 void Credit();
-void AddQueue();
+void AddQueue(Player P);
 void UpdatePeta();
 void GoUp(Player *P);
 void GoDown(Player *P);
 void GoLeft(Player *P);
 void GoRight(Player *P);
 void TampilPeta(int room);
+
+//Titipan Louis
+void tampilanAwal();
+//Menampilkan tampilan awal game
+void menu(int *X);
+//Menampilkan menu awal
+void newGame(TabInt *User, KataArray *nama);
+
+void loadGame(TabInt User, KataArray *nama, int *indexUser);
+
 #endif
